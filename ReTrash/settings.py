a3942b04768ca130aa4ba0e9e1c42e2a91f06d2f
hyoEuoXa <vladyslav.fliahin@nure.ua>
2021-05-30 19:06:55 +0300

    fdsf

--- ReTrash/settings.py
+++ ReTrash/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ReTrash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR + 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ReTrash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + 'db.sqlite3',
     }
 }
 
